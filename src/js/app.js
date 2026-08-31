c99cc8f5e273b5b20ed3e79adccfb13c056653bc
lianet <jack.tolmak@yandex.ru>
2026-08-31 12:15:05 +0300

    fix: layout modals

--- src/js/app.js
+++ src/js/app.js
@@ -679,6 +679,81 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 
+	// phone mask
+	const phoneInputs = document.querySelectorAll('input[type="tel"]');
+
+	function getInputNumbersValue(input) {
+		return input.value.replace(/\D/g, "");
+	}
+
+	function onPhonePaste(e) {
+		const input = e.target;
+		const inputNumbersValue = getInputNumbersValue(input);
+		const pasted = e.clipboardData || window.clipboardData;
+		if (pasted) {
+			const pastedText = pasted.getData("Text");
+			if (/\D/g.test(pastedText)) {
+				input.value = inputNumbersValue;
+			}
+		}
+	}
+
+	function onPhoneInput(e) {
+		const input = e.target;
+		let inputNumbersValue = getInputNumbersValue(input);
+		const selectionStart = input.selectionStart;
+		let formattedInputValue = "";
+
+		if (!inputNumbersValue) {
+			input.value = "";
+			return;
+		}
+
+		if (input.value.length !== selectionStart) {
+			if (e.data && /\D/g.test(e.data)) {
+				input.value = inputNumbersValue;
+			}
+			return;
+		}
+
+		if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+			if (inputNumbersValue[0] === "9") {
+				inputNumbersValue = "7" + inputNumbersValue;
+			}
+			const firstSymbols = inputNumbersValue[0] === "8" ? "8" : "+7";
+			formattedInputValue = firstSymbols + " ";
+			if (inputNumbersValue.length > 1) {
+				formattedInputValue += "(" + inputNumbersValue.substring(1, 4);
+			}
+			if (inputNumbersValue.length >= 5) {
+				formattedInputValue += ") " + inputNumbersValue.substring(4, 7);
+			}
+			if (inputNumbersValue.length >= 8) {
+				formattedInputValue += "-" + inputNumbersValue.substring(7, 9);
+			}
+			if (inputNumbersValue.length >= 10) {
+				formattedInputValue += "-" + inputNumbersValue.substring(9, 11);
+			}
+		} else {
+			formattedInputValue = "+" + inputNumbersValue.substring(0, 16);
+		}
+
+		input.value = formattedInputValue;
+	}
+
+	function onPhoneKeyDown(e) {
+		const inputValue = e.target.value.replace(/\D/g, "");
+		if (e.key === "Backspace" && inputValue.length === 1) {
+			e.target.value = "";
+		}
+	}
+
+	phoneInputs.forEach((phoneInput) => {
+		phoneInput.addEventListener("keydown", onPhoneKeyDown);
+		phoneInput.addEventListener("input", onPhoneInput);
+		phoneInput.addEventListener("paste", onPhonePaste);
+	});
+
 	// helpers
 	function toggleReviewExpand(button) {
 		const card = button.closest(".review-card");
